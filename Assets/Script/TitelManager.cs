d2d53c6fa41bc9d01ee65ccd2c431b28851ea94a
K_H\f20da <214336728+yuno- 666@users.noreply.github.com>
2026-02-09 15:43:51 +0900

    title追加

---
+++ Assets/Script/TitelManager.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitelManager : MonoBehaviour
+{
+    public string sceneName;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Load()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
