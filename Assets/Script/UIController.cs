2bb7792e50119c7e769af23e15bb75781597d3a0
K_H\f20da <214336728+yuno- 666@users.noreply.github.com>
2026-02-12 11:46:27 +0900

    タイムバー追加

--- Assets/Script/UIController.cs
+++ Assets/Script/UIController.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -10,12 +11,36 @@ public class UIController : MonoBehaviour
     public GameObject restartButton;    // RESTARTボタン
     public GameObject nextButton;       // NEXTボタン
 
+    // 時間制限追加
+    public GameObject timeBar;  // 時間表示バー
+    public GameObject timeText;   // 時間テキスト
+    TimeController timeController;  // TimeController
+    bool useTime = true; // 時間制限を使うかどうかのフラグ
+
+    // プレイヤー情報
+    GameObject player;
+    PlayerController playerController;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         Invoke("InactiveImage", 1.0f);  // 1秒後に画像を非表示にする
         panel.SetActive(false);         // パネルを非表示にする
+
+
+        // 時間制限のプログラム
+        timeController = GetComponent<TimeController>();   // TimeControllerを取得
+        if (timeController != null)
+        {
+            if (timeController.gameTime == 0.0f) //もしgameTimeがもともと0なら時間制限は設けない
+            {
+                timeBar.SetActive(false);   // 制限時間なしなら隠す
+                useTime = false;　//時間制限を使わないフラグ
+            }
+        }
+        //プレイヤー情報とPlayerControllerコンポーネントの取得
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerController = player.GetComponent<PlayerController>();
 
     }
 
@@ -40,6 +65,11 @@ public class UIController : MonoBehaviour
             mainImage.GetComponent<Image>().sprite = gameClearSpr;  // 画像を設定する
 
             // 時間カウントを停止
+
+            if (timeController != null)
+            {
+                timeController.IsTimeOver(); //停止フラグON
+            }
         }
         else if (GameManager.gameState == GameState.GameOver)
         {
@@ -50,7 +80,37 @@ public class UIController : MonoBehaviour
             Button bt = nextButton.GetComponent<Button>();
             bt.interactable = false;
             mainImage.GetComponent<Image>().sprite = gameOverSpr;       // 画像を設定する
+            // 時間カウントを停止
+
+            if (timeController != null)
+            {
+                timeController.IsTimeOver(); //停止フラグON
+            }
         }
 
+
+
+        else if (GameManager.gameState == GameState.InGame)
+        {
+            if (player == null) { return; }
+
+            if (timeController != null && useTime)
+            {
+                // float型のUI用表示変数を取得し、整数に型変換することで小数を切り捨てる
+                int time = (int)timeController.GetDisplayTime();
+                // タイム更新
+                timeText.GetComponent<TextMeshProUGUI>().text = time.ToString();
+
+                if (useTime && timeController.isCountDown && time <= 0) //カウントダウンモードで時間が0なら
+                {
+                    playerController.GameOver(); // ゲームオーバーにする
+                }
+                else if (useTime && !timeController.isCountDown && time >= timeController.gameTime) //カウントアップモードで制限時間を超えたら
+                {
+                    playerController.GameOver(); // ゲームオーバーにする 
+                }
+
+            }
+        }
     }
 }
